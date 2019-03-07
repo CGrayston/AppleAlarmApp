1779388aad559acca424f8e8e3425664a01257ae
Deniz Tutucu <denizsafaktutuncu@gmail.com>
2019-03-07 11:09:16 -0700

    create Deniz Class

---
+++ AppleAlarmApp/Deniz.swift
@@ -0,0 +1,14 @@
+//
+//  Deniz.swift
+//  AppleAlarmApp
+//
+//  Created by Deniz Tutuncu on 3/7/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+import Foundation
+
+
+class Deniz {
+    
+}
